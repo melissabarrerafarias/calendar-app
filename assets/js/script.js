081b75d5d3d9224278125c7efabf20056033a8c3
melissikiss <melissikiss@gmail.com>
2020-10-15 22:53:42 -0700

    fix scope issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,21 @@ var date = moment().format('LLLL');
 console.log(date);
 $("#currentDay").append(date);
 
+var saveTasks = function () {
+    localStorage.setItem(textInput);
+};
+
 // time blocks
 $(".col-10").on("click", "p", function () {
     var text = $(this)
         .text()
         .trim();
-        var textInput = $("<textarea>")
+    var textInput = $("<textarea>")
         .addClass("form-control")
         .val(text);
-        $(this).replaceWith(textInput);
-        });
+    $(this).replaceWith(textInput);
+});
+
+$(":button").on("click", function () {
+    saveTasks();
+})
