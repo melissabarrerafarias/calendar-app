270520b80e6bae474ab628f4f277ee3734fe9e6a
melissikiss <melissikiss@gmail.com>
2020-10-16 12:39:07 -0700

    finally finished layout + buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,6 @@ var date = moment().format('LLLL');
 console.log(date);
 $("#currentDay").append(date);
 
-var saveTasks = function () {
-    localStorage.setItem(textInput);
-};
-
 // time blocks
 $(".col-10").on("click", "p", function () {
     var text = $(this)
@@ -18,6 +14,20 @@ $(".col-10").on("click", "p", function () {
     $(this).replaceWith(textInput);
 });
 
-$(":button").on("click", function () {
-    saveTasks();
-})
+// $(".saveBtn").on("click", function() {
+//     textInput.val = $(this)
+//     .val()
+//     .trim();
+    
+//     console.log(textInput)
+
+// })
+
+// var to get item from local storage
+//  var savedToDo = JSON.parse(localStorage.getItem('ToDo')) || [];
+
+// $(".save-btn").on("click", function () {
+    //     console.log("hey this is working!");
+    //     localStorage.setItem("ToDo", JSON.stringify(textInput));
+    //     JSON.parse(localStorage.getItem('ToDo')) || [];
+    // })
