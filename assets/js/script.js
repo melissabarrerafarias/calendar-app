f25b9dd33b6ba2a869c44d681c2395c5ebfc1c88
melissikiss <melissikiss@gmail.com>
2020-10-15 17:10:33 -0700

    kinda solved textarea issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,21 @@ console.log(date);
 $("#currentDay").append(date);
 
 // time blocks
-$(".col-10").on("click", "p", function() {
-    console.log(this);
+$(".col-10").on("click", "p", function () {
+    var text = $(this)
+        .text()
+        .trim();
+        var textInput = $("<textarea>")
+        .addClass("form-control")
+        .val(text);
+        $(this).replaceWith(textInput);
+
+        $(".col-10").on("blur", "textarea", function() {
+            var text = $(this)
+            .val()
+            .trim();
+
+            
+            
+        });
 })
